da6e001a8db85f8b60c75930b333be909ec44ec9
SpartaAcademy <spartatulcan@gmail.com>
2025-12-11 14:56:52 -0500

    Update script-resultados.js

--- JS/script-resultados.js
+++ JS/script-resultados.js
@@ -1,6 +1,6 @@
 // JS/script-resultados.js
 
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
 
 const supabaseUrl = 'https://vwfpjvfjmmwmrqqahooi.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3ZnBqdmZqbW13bXJxcWFob29pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0NzkyNTcsImV4cCI6MjA4MTA1NTI1N30.pTc8KM-GnxVRgrYpcqm8YUZ9zb6Co-QgKT0i7W41HEA';
