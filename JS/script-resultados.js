7124a8911a17bd74a380b93c95c1a35833820593
SpartaAcademy <spartatulcan@gmail.com>
2025-12-12 16:16:05 -0500

    Update script-resultados.js

--- JS/script-resultados.js
+++ JS/script-resultados.js
@@ -4,6 +4,7 @@
 const supabaseUrl = 'https://fgpqioviycmgwypidhcs.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZncHFpb3ZpeWNtZ3d5cGlkaGNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0OTkwMDgsImV4cCI6MjA4MTA3NTAwOH0.5ckdzDtwFRG8JpuW5S-Qi885oOSVESAvbLoNiqePJYo';
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const { jsPDF } = window.jspdf;
 
 
 
